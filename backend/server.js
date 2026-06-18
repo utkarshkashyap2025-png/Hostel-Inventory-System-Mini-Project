4920b7c6518762a842f277d871baf00ef5f1fe98
Utkarsh Kashyap <utkarsh.kashyap2025@glbajajgroup.org>
2026-06-19 01:14:44 +0530

    Completed dashboard module

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const roomRoutes = require('./routes/roomRoutes');
 
+
+const maintenanceRoutes = require('./routes/maintenanceRoutes');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const notificationRoutes = require('./routes/notificationRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -22,6 +30,11 @@ app.use('/api/rooms', roomRoutes);
 app.use('/api/requests', requestRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 //app.use('/api/inventory', inventoryRoutes);
+app.use('/api/maintenance', maintenanceRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/notifications', notificationRoutes);
+
 
 const startServer = async () => {
   try {
